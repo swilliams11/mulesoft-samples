c29da0c9db4c3ff79f4c65aa16a229d67c67f744
Sean Williams <sean.williams.11@gmail.com>
2023-07-06 14:44:37 -0500

    adding java callout example

---
+++ 26javacallout/README.md
@@ -0,0 +1,10 @@
+# Java Callout
+
+```
+curl -X POST "localhost:8081/java?firstName=S&lastName=L"
+```
+
+```
+curl -X GET "localhost:8081/nonstaticjava?fName=john&mName=Peter&lName=L&suffix=III"
+```
+
---
+++ 26javacallout/src/main/java/StringOperations.java
@@ -0,0 +1,16 @@
+
+public class StringOperations {
+	/**
+	 * Combines two strings.
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static String concat(String a, String b) {
+		return a + " " + b;
+	}
+	
+	public String multiConcat(String a, String b, String c, String d) {
+		return a + " " + b + " " + c + " " + d;
+	}
+}
